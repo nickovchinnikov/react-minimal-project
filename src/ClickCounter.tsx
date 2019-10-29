f119a1438d4cbcffe4efb65427c38019acc52d7d
NickMasters <nickovchinnikof@gmail.com>
2019-10-29 16:53:20 +0300

    Add ClickCounter

---
+++ src/ClickCounter.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+interface IProps { }
+
+interface IState {
+  count: number
+  name: string
+}
+
+export class ClickCounter extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: 'viktor'
+    };
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+    const { count } = this.state;
+    this.setState({ count: count + 1 })
+  }
+
+  render() {
+    // используем state в render()
+    const { count } = this.state;
+    // изменяем состояние через setState
+    return (<button onClick={this.increment}>
+      Clicked {count} times!
+    </button>);
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom';
 
-function tick() {
-  const element = (
-    <div style={{ color: "red" }}>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  render(element, document.getElementById('root'));
-}
+import { ClickCounter } from './ClickCounter';
 
-setInterval(tick, 1000);
+render(<ClickCounter />, document.getElementById('root'));
