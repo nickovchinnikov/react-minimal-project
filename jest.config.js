83cc3b314f02705db9035076779e0f95990c5572
NickMasters <nickovchinnikof@gmail.com>
2019-11-07 12:37:03 +0300

    Add jest and enzyme conf

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   testEnvironment: "node",
+  "setupFilesAfterEnv": ["<rootDir>/internals/jest/settings.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
