53d2ddd2890ab042fcbd3ae8ecd16594972d4ecf
NickOvchinnikov <nickovchinnikof@gmail.com>
2020-07-23 18:56:42 +0300

    Redux logger middleware

--- src/redux/counter/index.tsx
+++ src/redux/counter/index.tsx
@@ -1,20 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+  Dispatch,
+  Action
+} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { Counter } from './components'
 import counter, { Actions } from './reducers'
 
-const store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-const rootEl = document.getElementById('root')
+const reducer = combineReducers({ counter });
+export type CounterAppState = ReturnType<typeof reducer>;
 
-export const CounterApp = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: Actions.INC })}
-    onDecrement={() => store.dispatch({ type: Actions.DEC })}
-  />,
-  rootEl
-)
+const logger = (store: any) => (next: Dispatch) => (action: Action) => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result;
+};
 
-store.subscribe(CounterApp)
+const enhancer = composeWithDevTools(
+  applyMiddleware(logger)
+);
+
+const store = createStore(reducer, enhancer);
+
+const rootEl = document.getElementById('root');
+
+export const CounterApp = () => {
+  const { counter } = store.getState();
+  return ReactDOM.render(
+    <Counter
+      value={counter}
+      onIncrement={() => store.dispatch({ type: Actions.INC })}
+      onDecrement={() => store.dispatch({ type: Actions.DEC })}
+    />,
+    rootEl
+  );
+};
+
+store.subscribe(CounterApp);
