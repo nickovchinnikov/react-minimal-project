b3e928caa34d70df625a435c4340d20a0cd2f54f
NickOvchinnikov <nickovchinnikof@gmail.com>
2020-11-10 09:55:25 +0300

    Hook examples

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,13 @@ import {
 
 import { Gallery } from './patterns/Gallery';
 import { GiphySearch } from './patterns/GiphySearch';
-import { ClickCounter } from './patterns/ClickCounterHOC';
+// import { ClickCounter } from './patterns/ClickCounterHOC';
+import { ClickCounter } from './patterns/ClickCounter';
+import { ClickCounterHooks } from './patterns/ClickCounterHooks'
+import { CounterReducer } from './patterns/CounterReducer';
 
 import { RedditApp } from './redux/reddit/index'
+import { Counter } from './redux/counter/components';
 
 const cats = [
   'https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg',
@@ -52,6 +56,15 @@ const App = () => <Router>
           <Link to="/click-counter">Click-counter</Link>
         </li>
         <li>
+          <Link to="/click-counter">Click-counter</Link>
+        </li>
+        <li>
+          <Link to="/click-counter-hooks">Click-counter hooks</Link>
+        </li>
+        <li>
+          <Link to="/counter-reducer">Counter-Reducer</Link>
+        </li>
+        <li>
           <Link to="/gallery">Gallery</Link>
         </li>
         <li>
@@ -69,6 +82,12 @@ const App = () => <Router>
       } />
       <Route path="/click-counter">
         <ClickCounter />
+      </Route>
+      <Route path="/click-counter-hooks">
+        <ClickCounterHooks defaultCount={10} />
+      </Route>
+      <Route path="/counter-reducer">
+        <CounterReducer />
       </Route>
       <Route path="/reddit">
         <RedditApp />
--- src/patterns/ClickCounterHooks.tsx
+++ src/patterns/ClickCounterHooks.tsx
@@ -1,19 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useCallback } from 'react';
 
-export function ClickCounterHooks() {
+interface Props {
+  defaultCount?: number
+}
+
+const ClickCallback = () => {
+  console.warn("Clicked!")
+}
+
+export const ClickCounterHooks: FC<Props> = ({ defaultCount = 0 }) => {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(defaultCount);
+
+  const [squredCount, setSquredCount] = useState(Math.pow(defaultCount, 2));
+
+  useEffect(() => {
+    document.addEventListener("click", ClickCallback)
+  });
 
   useEffect(() => {
     // Update the document title using the browser API
     document.title = `${count} times`;
-  });
+  }, [count]);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.warn(`${squredCount} squredCount`);
+  }, [squredCount]);
+
+  const clickHandler = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+
+  const clickSquaredHandler = () => {
+    setSquredCount(Math.pow(count, 2))
+  }
 
   return (
     <div>
       <p>{count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={clickHandler}>
         Click me
+      </button>
+      <p>{squredCount} times (count squred!)</p>
+      <button onClick={clickSquaredHandler}>
+        Click me (count squred!)
       </button>
     </div>
   );
